379d6a17b3046b663e89ddb0cb96a1f08f6603d1
Jonathan <chon172009@live.com.mx>
2021-12-20 12:55:52 -0600

    Setting driver

--- src/SeleniumFirst/StaticDropDown.java
+++ src/SeleniumFirst/StaticDropDown.java
@@ -1,6 +1,17 @@
 package SeleniumFirst;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class StaticDropDown {
+
+    WebDriver driver;
+    public void Setwebdriver(){
+        driver = new EdgeDriver();
+        
+
+    }
+    
     public static void main(String[] args) {
         
     }
