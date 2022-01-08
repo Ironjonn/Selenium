e4c6e516ab5b30e0a6b4e86b3a6bc2ad64e2a93a
Jonathan <chon172009@live.com.mx>
2022-01-08 12:13:17 -0600

    Adding a few examples to the calendar is nice check the class for the examples

--- src/SeleniumFirst/CalendarUI.java
+++ src/SeleniumFirst/CalendarUI.java
@@ -1,24 +1,23 @@
 package SeleniumFirst;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class CalendarUI {
 
-    WebDriver mydriver; 
+    WebDriver mydriver;
 
-    public void launchBrowser(){
+    public void launchBrowser() {
         System.setProperty("webdriver.edge.driver", "C:\\\\Selenium\\\\edgedriver_win32\\\\msedgedriver.exe");
         mydriver = new EdgeDriver();
     }
 
-    public void GetUrl ( ){
+    public void GetUrl() {
         mydriver.get("https://rahulshettyacademy.com/dropdownsPractise/");
     }
 
-    public void Windowactivities(){
+    public void Windowactivities() {
         mydriver.manage().deleteAllCookies();
         mydriver.manage().window().maximize();
     }
@@ -28,28 +27,32 @@ public class CalendarUI {
         Thread.sleep(2000);
         mydriver.findElement(By.xpath("//a[@value='GOI']")).click();
         Thread.sleep(2000);
-        mydriver.findElement(By.xpath("(//a[@value='JAI'])[2]")).click(); 
-        Thread.sleep(1000L); 
-        mydriver.findElement(By.cssSelector(".ui-state-defaultui-state-highlightui-state-active")).click();
+        mydriver.findElement(By.xpath("(//a[@value='JAI'])[2]")).click();
+        Thread.sleep(1000L);
+        // mydriver.findElement(By.cssSelector("ui-state-default ui-state-highlight
+        // ui-state-active")).click();
+    }
+
+    public void Example() {
+
         
+        // ejemplos de abajo para seleccionar el dia en el calendario de 3 formas
+        // diferentes sirve mucho para repaso
+        mydriver.findElement(By.xpath("//*[text()='29']")).click();
+
+        // mydriver.findElement(By.xpath("//*[contains(@class,'ui-state-default')]")).click();
+        // mydriver.findElement(By.cssSelector(".ui-state-highlight")).click();
 
     }
 
-    public void Example(){
-        
-        mydriver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
-        
-       //System.out.println(mydriver.findElement(By.cssSelector("a[.ui-state-defaultui-state-highlight]")).getText());  
-        
-    }
     public static void main(String[] args) throws InterruptedException {
         CalendarUI objeto = new CalendarUI();
         objeto.launchBrowser();
         objeto.GetUrl();
         objeto.Windowactivities();
         objeto.FromTo();
-        //Thread.sleep(2000);
-       // objeto.Example();
+
+        objeto.Example();
         System.out.println("Hola mundo desde Visualstudiocode");
     }
 }
