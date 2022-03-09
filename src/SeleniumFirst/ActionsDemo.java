b6fd40658dcc465e0971da7efb27fd52990448c2
Jonathan <chon172009@live.com.mx>
2022-03-09 14:43:36 -0600

    Finishin the example of actions for no will check more in the futher \

--- src/SeleniumFirst/ActionsDemo.java
+++ src/SeleniumFirst/ActionsDemo.java
@@ -1,16 +1,31 @@
 package SeleniumFirst;
 
-import org.openqa.selenium.WebDriver;
 
+import java.time.Duration;
+
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.internal.thread.ThreadExecutionException;
 
 
 public class ActionsDemo {
+    //acctions es usado para monitorear el mouse doble click hover etc para otras funciones 
     WebDriver driver;
+    
     public void launchBrowser(){
         System.setProperty("webdriver.edge.driver", "C:\\\\Selenium\\\\edgedriver_win32\\\\msedgedriver.exe");
         driver = new EdgeDriver();
-
+        //Implicit wait se aplica globalmente que espere tantos segundos en encontrar el objecto 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         
     }
     public void getUrl(){
@@ -20,10 +35,40 @@ public class ActionsDemo {
         driver.manage().window().maximize();
     }
 
-    public static void main(String[] args) {
+    public void example (){
+        
+        
+        Actions a = new Actions(driver);
+        WebElement move = driver.findElement(By.xpath("//*[@class ='nav-a nav-a-2   nav-progressive-attribute']"));
+        
+        WebElement movei = driver.findElement(By.xpath("//*[@id ='twotabsearchtextbox']"));
+
+        
+       //context click es el right click 
+        a.moveToElement(move).contextClick().build().perform();
+        a.moveToElement(movei).click().keyDown(Keys.SHIFT).sendKeys("hellows").doubleClick().build().perform();
+    }
+
+    public void example2 () throws InterruptedException{
+        Thread.sleep(3000);
+        WebDriverWait waiting;
+        waiting = new WebDriverWait(driver, 20);
+        
+            
+         driver.navigate().to("https://rahulshettyacademy.com/loginpagePractise/#");
+         Thread.sleep(2000);
+         driver.findElement(By.xpath("//a[@href = 'https://rahulshettyacademy.com/#/documents-request']")).click();
+         //driver.navigate().back();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         ActionsDemo myobjt = new ActionsDemo();
         myobjt.launchBrowser();
         myobjt.getUrl();
         myobjt.windowsActivities();
+        myobjt.example();
+        myobjt.example2();
+
+
     }
 }
