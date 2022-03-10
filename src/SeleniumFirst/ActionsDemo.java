1f7570952af0be3b98d9cfe674d3628e4ff38162
Jonathan <chon172009@live.com.mx>
2022-03-10 15:12:12 -0600

    Continue working with windowHandle

--- src/SeleniumFirst/ActionsDemo.java
+++ src/SeleniumFirst/ActionsDemo.java
@@ -1,10 +1,8 @@
 package SeleniumFirst;
 
 
-import java.time.Duration;
-
-
-
+import java.util.Iterator;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,53 +10,61 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.internal.thread.ThreadExecutionException;
+
 
 
 public class ActionsDemo {
-    //acctions es usado para monitorear el mouse doble click hover etc para otras funciones 
+    // acctions es usado para monitorear el mouse doble click hover etc para otras
+    // funciones
     WebDriver driver;
-    
-    public void launchBrowser(){
+
+    public void launchBrowser() {
         System.setProperty("webdriver.edge.driver", "C:\\\\Selenium\\\\edgedriver_win32\\\\msedgedriver.exe");
         driver = new EdgeDriver();
-        //Implicit wait se aplica globalmente que espere tantos segundos en encontrar el objecto 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // Implicit wait se aplica globalmente que espere tantos segundos en encontrar
+        // el objecto
         
+
     }
-    public void getUrl(){
-        driver.get("https://www.amazon.com.mx/");
+
+    public void getUrl() {
+        driver.navigate().to("https://www.amazon.com.mx/");
     }
-    public void windowsActivities(){
+
+    public void windowsActivities() {
         driver.manage().window().maximize();
     }
 
-    public void example (){
-        
-        
+    public void example() {
+
         Actions a = new Actions(driver);
         WebElement move = driver.findElement(By.xpath("//*[@class ='nav-a nav-a-2   nav-progressive-attribute']"));
-        
+
         WebElement movei = driver.findElement(By.xpath("//*[@id ='twotabsearchtextbox']"));
 
-        
-       //context click es el right click 
+        // context click es el right click
         a.moveToElement(move).contextClick().build().perform();
         a.moveToElement(movei).click().keyDown(Keys.SHIFT).sendKeys("hellows").doubleClick().build().perform();
     }
 
-    public void example2 () throws InterruptedException{
+    public void example2() throws InterruptedException {
         Thread.sleep(3000);
-        WebDriverWait waiting;
-        waiting = new WebDriverWait(driver, 20);
         
-            
-         driver.navigate().to("https://rahulshettyacademy.com/loginpagePractise/#");
-         Thread.sleep(2000);
-         driver.findElement(By.xpath("//a[@href = 'https://rahulshettyacademy.com/#/documents-request']")).click();
-         //driver.navigate().back();
+        driver.navigate().to("https://rahulshettyacademy.com/loginpagePractise/#");;
+        
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+        Set <String> windows = driver.getWindowHandles();
+        Iterator <String> it =  windows.iterator();
+        String parentID = it.next();
+        String childID = it.next();
+        driver.switchTo().window(childID);
+        
+        System.out.print(driver.findElement(By.xpath("//*[@class='im-para red']")).getText());  
+        //driver.findElement(By.xpath("//*[text()= 'Free Access to InterviewQues/ResumeAssistance/Material']")).click();
+        // driver.navigate().back();
+        //driver.findElement(By.xpath("//a[@href = 'https://rahulshettyacademy.com/#/documents-request']")).click();
+
+
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -67,8 +73,8 @@ public class ActionsDemo {
         myobjt.getUrl();
         myobjt.windowsActivities();
         myobjt.example();
-        myobjt.example2();
 
+        myobjt.example2();
 
     }
 }
--- src/SeleniumFirst/WindowHandle.java
+++ src/SeleniumFirst/WindowHandle.java
@@ -1,43 +1,63 @@
 package SeleniumFirst;
 
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-
 public class WindowHandle {
 
-    WebDriver driver; 
+    WebDriver driver;
 
-    public void launchBrowser(){
+    public void launchBrowser() {
         System.setProperty("webdriver.edge.driver", "C:\\\\Selenium\\\\edgedriver_win32\\\\msedgedriver.exe");
         driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
     }
 
-        public void getUrl(){
-            driver.navigate().to("https://rahulshettyacademy.com/loginpagePractise/#");
-        }
+    public void getUrl() {
+        driver.navigate().to("https://rahulshettyacademy.com/loginpagePractise/#");
+    }
 
-        public void windowsActivities(){
-            driver.manage().window().maximize();
-        }
+    public void windowsActivities() {
+        driver.manage().window().maximize();
+    }
 
+    public void example() {
 
-        public void example () throws InterruptedException{
-            Thread.sleep(3000);
-           
-            
-                
-             
-             Thread.sleep(2000);
-             driver.findElement(By.xpath("//a[@href = 'https://rahulshettyacademy.com/#/documents-request']")).click();
-             //driver.navigate().back();
-        }
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+
+        // Aqui guardamos las ventanas en un string como si las guardaramos por index
+        // como un array teniendolas en un orden padre siendo la 1 y child la 2 y asi
+        // sucesibamente
+        // sirve para cuando cambias de pagina y el driver le tienes que decir en que
+        // pagina va buscar el locator
+        Set<String> windows = driver.getWindowHandles();
+        Iterator<String> it = windows.iterator();
+        String parentWindow = it.next();
+        String childWindow = it.next();
+        // driver.findElement(By.xpath("//a[@href =
+        // 'https://rahulshettyacademy.com/#/documents-request']")).click();
+        // driver.navigate().back();
+
+        // Aqui abajo le decimos que cambie a la ventana dos de las que tienes abiertas
+        // y que en esa ventana busque los locator y ya no este en la primera pagina
+        driver.switchTo().window(childWindow);
+        //En es css selector de abajo como tiene un espacio la palabra se le agrega un punto para que funcione 
+        String emailAdress = driver.findElement(By.cssSelector(".im-para.red")).getText();
+
+        System.out.print(emailAdress);
+
+    }
+
     public static void main(String[] args) {
-            WindowHandle obj = new WindowHandle();
-            obj.launchBrowser();
-            obj.getUrl();
-            obj.windowsActivities();
-
+        WindowHandle obj = new WindowHandle();
+        obj.launchBrowser();
+        obj.getUrl();
+        obj.windowsActivities();
+        obj.example();
     }
 }
