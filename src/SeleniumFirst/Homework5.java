d2fd0be2fdcf29a41f7fec94d395953d3a92c423
Jonathan <chon172009@live.com.mx>
2022-03-16 12:47:11 -0600

    Ading Homework 5 class

---
+++ src/SeleniumFirst/Homework5.java
@@ -0,0 +1,35 @@
+package SeleniumFirst;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Homework5 {
+
+    WebDriver driver;
+
+    public void launchBrowser(){
+        System.setProperty("webdriver.edge.driver", "C:\\\\Selenium\\\\edgedriver_win32\\\\msedgedriver.exe");
+        driver = new EdgeDriver();
+    }
+
+    public void getUrl(){
+        driver.navigate().to("https://the-internet.herokuapp.com");
+    }
+
+    public void windowsActivities(){
+        driver.manage().window().maximize();
+    }
+
+    public void homework(){
+        driver.findElement(By.xpath("//*[@ ]");
+    }
+
+    public static void main(String[] args) {
+        Homework5 obx = new Homework5();
+        obx.launchBrowser();
+        obx.getUrl();
+        obx.windowsActivities();
+
+    }
+}
