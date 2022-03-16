4ef787901a3baf62712afa094dcf1201f1b21002
Jonathan <chon172009@live.com.mx>
2022-03-16 13:28:24 -0600

    finishing the homework 5 Nested Frames see the example

--- src/SeleniumFirst/Homework5.java
+++ src/SeleniumFirst/Homework5.java
@@ -1,5 +1,7 @@
 package SeleniumFirst;
 
+import javax.swing.JOptionPane;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -22,7 +24,14 @@ public class Homework5 {
     }
 
     public void homework(){
-        driver.findElement(By.xpath("//*[@ ]");
+        driver.findElement(By.xpath("//*[text() = 'Frames']")).click();
+
+        driver.findElement(By.xpath("//*[text() = 'Nested Frames']")).click();
+
+        driver.switchTo().frame("frame-top").switchTo().frame("frame-middle");
+
+        String frameName = driver.findElement(By.xpath("//div[text() = 'MIDDLE']")).getText();
+       JOptionPane.showMessageDialog(null, frameName); 
     }
 
     public static void main(String[] args) {
@@ -30,6 +39,7 @@ public class Homework5 {
         obx.launchBrowser();
         obx.getUrl();
         obx.windowsActivities();
+        obx.homework();
 
     }
 }
