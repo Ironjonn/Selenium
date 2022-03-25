202b85afc281fe99cfb57c91d277974b32f5e09b
Jonathan <chon172009@live.com.mx>
2022-03-25 12:03:32 -0600

    Finishing the homework 6 check it if you need some java knolegment

--- src/SeleniumFirst/Homework6.java
+++ src/SeleniumFirst/Homework6.java
@@ -1,5 +1,7 @@
 package SeleniumFirst;
 
+
+
 import javax.swing.JOptionPane;
 
 import org.openqa.selenium.By;
@@ -9,7 +11,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.WebElement;
 
 public class Homework6 {
-    
+
     WebDriver driver;
 
     public void launchBrowser() {
@@ -20,35 +22,46 @@ public class Homework6 {
 
     public void getUrl() {
         driver.navigate().to("https://www.rahulshettyacademy.com/AutomationPractice/");
-        
+
     }
 
     public void windowsActivities() {
         driver.manage().window().maximize();
     }
 
-    public void example () throws InterruptedException{
-        WebElement dhead = driver.findElement(By.xpath("//div[@id ='checkbox-example']"));
-        dhead.findElement(By.xpath("//*[@id = 'checkBoxOption2']")).click();
-        
-        String name = dhead.findElement(By.xpath("//label[@for='benz']")).getText();
-        JOptionPane.showMessageDialog(null, name);
+    public void example() throws InterruptedException {
 
-        
-        Thread.sleep(2000);
+        WebElement dhead = driver.findElement(By.xpath("//div[@id ='checkbox-example']"));
+        dhead.findElement(By.xpath("//*[@id='checkbox-example']/fieldset/label[2]/input")).click();
+
+        String name = dhead.findElement(By.xpath("//*[@id='checkbox-example']/fieldset/label[2]")).getText();
+        JOptionPane.showMessageDialog(null, name);
 
         WebElement dropdown = driver.findElement(By.xpath("//*[@id ='dropdown-class-example']"));
         Select dri = new Select(dropdown);
 
-        dri.selectByIndex(2);
+        dri.selectByVisibleText(name);
 
+        driver.findElement(By.xpath("//*[@name = 'enter-name']")).sendKeys(name);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//*[@id ='alertbtn']")).click();
+        String name2 = driver.switchTo().alert().getText();
+
+        if (name2.contains(name)) {
+            System.out.println("pass successfully");
+        } else {
+            System.out.println("failed, check the code");
+        }
+
+        driver.switchTo().alert().accept();
     }
+
     public static void main(String[] args) throws InterruptedException {
         Homework6 obj = new Homework6();
         obj.launchBrowser();
         obj.getUrl();
         obj.windowsActivities();
         obj.example();
-        
+
     }
 }
