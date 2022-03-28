e418e34c125a8494355c6ccc8530cddc035bad7c
Jonathan <chon172009@live.com.mx>
2022-03-28 14:44:01 -0600

    Still working on the days of the calendar

--- src/SeleniumFirst/HandilingCalendarUI.java
+++ src/SeleniumFirst/HandilingCalendarUI.java
@@ -1,6 +1,12 @@
 package SeleniumFirst;
 
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class HandilingCalendarUI {
@@ -21,15 +27,37 @@ public class HandilingCalendarUI {
         driver.manage().window().maximize();
     }
 
-    public void example(){
+    public void gettingdays(){
+        
+        String insertday = JOptionPane.showInputDialog(null, "Please insert the day you want to select in the calendar");
 
+
+        driver.findElement(By.xpath("//*[@name = 'travel_date']")).click();
+        //driver.findElement(By.xpath("//input[contains(@id,'friendsa')]")).click();
+
+       List <WebElement> days = driver.findElements(By.xpath("//td[contains(@class,'day')]"));
+       int dayssize = days.size();
+       System.out.println(dayssize);
+       for(int i = 0; i<dayssize; i++){
+          String daysname =   days.get(i).getText();
+
+          if(daysname.equalsIgnoreCase(insertday)){
+            days.get(i).click();
+
+            break;
+          }
+       }
+    }
+
+    public void gettingmonths (){
+        
     }
     public static void main(String[] args) {
         HandilingCalendarUI obx = new HandilingCalendarUI();
         obx.launchBrowser();
         obx.getUrl();
         obx.windowsActivities();
-        obx.example();
+        obx.gettingdays();
 
     }
 }
