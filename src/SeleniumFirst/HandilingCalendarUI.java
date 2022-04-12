59d1e94317d6e688fbcb0922b3bcf41c25eace46
Jonathan <chon172009@live.com.mx>
2022-04-12 18:05:04 -0500

    Still working on the skrollingdown

--- src/SeleniumFirst/HandilingCalendarUI.java
+++ src/SeleniumFirst/HandilingCalendarUI.java
@@ -1,6 +1,6 @@
 package SeleniumFirst;
 
-import java.util.ArrayList;
+
 //  import java.time.Duration;
 import java.util.List;
 
--- src/SeleniumFirst/HowtoHandleSkrolling.java
+++ src/SeleniumFirst/HowtoHandleSkrolling.java
@@ -1,7 +1,16 @@
 package SeleniumFirst;
 
+import java.util.List;
+
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.JOptionPane;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.WebElement;
+
 
 public class HowtoHandleSkrolling {
 
@@ -18,17 +27,59 @@ public class HowtoHandleSkrolling {
     }
 
     public void getUrl() {
-        driver.navigate().to("https://www.amazon.com.mx/");
+        driver.navigate().to("https://www.rahulshettyacademy.com/AutomationPractice/");
     }
 
     public void windowsActivities() {
         driver.manage().window().maximize();
     }
 
-    public void example(){
+    public void example() throws InterruptedException{
+        JavascriptExecutor objx = (JavascriptExecutor)driver;
+
+        objx.executeScript("window.scrollBy(0,200)");
+        Thread.sleep(2000);
+
+        objx.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+        List<WebElement> table = driver.findElements(By.xpath("//*[@class = 'tableFixHead']//td[4]"));
+
+        int sum = 0;
+        
+        for(int i = 0; i<table.size(); i++){
+            
+            String tablesize = table.get(i).getText();
+          
+          
+            sum = sum + Integer.parseInt(tablesize);
+
+            
+            
+        }
+                    
+        JOptionPane.showMessageDialog(null, sum);
+
+        String name =  driver.findElement(By.xpath("//*[@class = 'totalAmount']")).getText();
+        //Total Amount Collected: 296
+        
+        String[] namearray = name.split(":");
+        String number =  namearray[1];
+        String amount = number.trim();
+        System.out.println(amount);
+
+        String value =  String.valueOf(sum);
+       
+        if(amount.equals(value)){
+            JOptionPane.showMessageDialog(null, "Your successfully loged in congrats ");
+        }else{
+            JOptionPane.showMessageDialog(null, "Your failed the completed amount ");
+        }
+
+        
+        
 
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         HowtoHandleSkrolling obj = new HowtoHandleSkrolling();
         obj.launchBrowser();
         obj.getUrl();
