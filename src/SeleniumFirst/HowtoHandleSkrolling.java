5306429a7976f6915a30020a9b67da73ad8531ef
Jonathan <chon172009@live.com.mx>
2022-04-13 18:12:17 -0500

    Finishing the homework 7, will send to review

--- src/SeleniumFirst/HowtoHandleSkrolling.java
+++ src/SeleniumFirst/HowtoHandleSkrolling.java
@@ -2,18 +2,18 @@ package SeleniumFirst;
 
 import java.util.List;
 
-import javax.print.attribute.standard.JobHoldUntil;
 import javax.swing.JOptionPane;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import org.testng.Assert;
+
 import org.openqa.selenium.WebElement;
 
-
 public class HowtoHandleSkrolling {
-
 
     WebDriver driver;
 
@@ -22,7 +22,6 @@ public class HowtoHandleSkrolling {
         driver = new EdgeDriver();
         // Implicit wait se aplica globalmente que espere tantos segundos en encontrar
         // el objecto
-        
 
     }
 
@@ -34,8 +33,8 @@ public class HowtoHandleSkrolling {
         driver.manage().window().maximize();
     }
 
-    public void example() throws InterruptedException{
-        JavascriptExecutor objx = (JavascriptExecutor)driver;
+    public void example() throws InterruptedException {
+        JavascriptExecutor objx = (JavascriptExecutor) driver;
 
         objx.executeScript("window.scrollBy(0,200)");
         Thread.sleep(2000);
@@ -45,45 +44,84 @@ public class HowtoHandleSkrolling {
         List<WebElement> table = driver.findElements(By.xpath("//*[@class = 'tableFixHead']//td[4]"));
 
         int sum = 0;
-        
-        for(int i = 0; i<table.size(); i++){
-            
+
+        for (int i = 0; i < table.size(); i++) {
+
             String tablesize = table.get(i).getText();
-          
-          
+
             sum = sum + Integer.parseInt(tablesize);
 
-            
-            
         }
-                    
+
         JOptionPane.showMessageDialog(null, sum);
 
-        String name =  driver.findElement(By.xpath("//*[@class = 'totalAmount']")).getText();
-        //Total Amount Collected: 296
-        
+        String name = driver.findElement(By.xpath("//*[@class = 'totalAmount']")).getText();
+        // Total Amount Collected: 296
+
         String[] namearray = name.split(":");
-        String number =  namearray[1];
+        String number = namearray[1];
         String amount = number.trim();
         System.out.println(amount);
 
-        String value =  String.valueOf(sum);
-       
-        if(amount.equals(value)){
+        String value = String.valueOf(sum);
+
+        if (amount.equals(value)) {
             JOptionPane.showMessageDialog(null, "Your successfully loged in congrats ");
-        }else{
+        } else {
             JOptionPane.showMessageDialog(null, "Your failed the completed amount ");
         }
 
-        
-        
-
     }
+    public static  int total1;
+    private void secondtable() {
+        List<WebElement> table2 = driver.findElements(By.xpath("//table[@class = 'table-display']//td[3]"));
+
+        int summ = 0;
+        for (int t = 0; t < table2.size(); t++) {
+            String tablefull = table2.get(t).getText();
+
+            summ = summ + Integer.parseInt(tablefull);
+        }
+
+        JOptionPane.showMessageDialog(null, summ);
+
+        this.total1 = summ;
+
+        // Assert.assertEquals(summ, total);
+    }
+
+    public static int total2;
+    
+    public int total(){
+        
+        List<WebElement> table2 = driver.findElements(By.xpath("//table[@class = 'table-display']//td[3]"));
+         int sume = 0;
+        for(int y =0; y<table2.size(); y++){
+            String totale = table2.get(y).getText();
+
+            sume = sume + Integer.parseInt(totale);
+
+
+            this.total2 = sume;
+            
+        }
+        return total2;
+        
+    }
+
+    
+
     public static void main(String[] args) throws InterruptedException {
         HowtoHandleSkrolling obj = new HowtoHandleSkrolling();
         obj.launchBrowser();
         obj.getUrl();
         obj.windowsActivities();
         obj.example();
+
+        obj.secondtable();
+
+        obj.total();
+
+        Assert.assertEquals(total1, total2);
     }
 }
