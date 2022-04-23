abb7ecc211519267aee38c979ea534f850382f40
Jonathan <chon172009@live.com.mx>
2022-04-23 10:41:17 -0500

    Finishing the Httpcertification class, please review the Chromeoptios class for more options

--- src/SeleniumFirst/Httpcertifications.java
+++ src/SeleniumFirst/Httpcertifications.java
@@ -1,49 +1,39 @@
 package SeleniumFirst;
 
-
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Httpcertifications  {
+public class Httpcertifications {
+    // For more information you can go to
+    // https://chromedrivger.chromium.org/capabilities you can set where you want to
+    // donwload any thing or set the proxy this is yous an example of the class
+    // ChromeOptions, also block pop ups ventanitas que no quieres 
+
     WebDriver driver;
     ChromeOptions options = new ChromeOptions();
-    
-    
-    
-    public void launchBrowser(){
+
+    public void launchBrowser() {
         System.setProperty("webdriver.chrome.driver", "C:\\\\Selenium\\\\chromedriver.exe");
         options.setAcceptInsecureCerts(true);
         driver = new ChromeDriver(options);
 
-         
     }
 
-    public void geturl (){
+    public void geturl() {
         driver.navigate().to("https://expired.badssl.com/");
     }
 
     public void windowsActivities() {
-        
+
         driver.manage().window().maximize();
     }
 
-
-    
     public static void main(String[] args) {
         Httpcertifications obj = new Httpcertifications();
         obj.launchBrowser();
         obj.geturl();
         obj.windowsActivities();
-        
-    
-        
-       
-        
-
-
 
     }
 
--- src/SeleniumFirst/MainJava.java
+++ src/SeleniumFirst/MainJava.java
@@ -2,14 +2,13 @@ package SeleniumFirst;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class MainJava {
     WebDriver driver;
 
     public void launchBrowser(){
-        // ChromeOptions options = new ChromeOptions();
-        // options.setAcceptInsecureCerts(true);
+        
         System.setProperty("webdriver.chrome.driver", "C:\\\\Selenium\\\\chromedriver.exe");
          driver = new ChromeDriver();
          
