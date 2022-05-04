faf347bf273e588053e9adabddb40d7e1ab10833
Jonathan <chon172009@live.com.mx>
2022-05-04 13:25:53 -0500

    Adding the stream lambda exprexion using Map class

---
+++ src/StreamsandLambdaExpretionMap.java
@@ -0,0 +1,13 @@
+import com.beust.jcommander.MissingCommandException;
+
+public class StreamsandLambdaExpretionMap {
+
+    public void streamMap(){
+
+    }
+
+    public static void main(String[] args) {
+        StreamsandLambdaExpretionMap objeto = new StreamsandLambdaExpretionMap();
+        objeto.streamMap();
+    }
+}
